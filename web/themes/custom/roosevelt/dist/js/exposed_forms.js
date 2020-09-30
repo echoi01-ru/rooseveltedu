51cb4e85646aaffc16146d245e1aebd7e19ec130
Savino <jason@kanopistudios.com>
2020-09-30 08:19:26 -0400

    stories view and styles

---
+++ web/themes/custom/roosevelt/dist/js/exposed_forms.js
@@ -0,0 +1,13 @@
+/*
+ * @file Javascripts.
+ */
+(function ($, Drupal) {
+  Drupal.behaviors.storyFilterToggle = {
+    attach: function () {
+      $(".storyFilterToggle").once("storyFilterToggle").click(function () {
+        $(".storyFilterTogglePanel").slideToggle("slow");
+        $(".storyFilterToggle").toggleClass("open");
+      });
+    }
+  }
+})(jQuery, Drupal);
